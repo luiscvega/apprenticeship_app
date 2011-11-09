558a7f85a68e5eb94f5c7cfaafac59b531192cd7
Luis Vega <luiscvega@gmail.com>
2011-11-09 16:11:14 -0600

    Added Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApprenticeshipAppV2::Application.config.secret_token = 'b27f8f8deb98df0b4382424f27b4be7c5b3e899a1fed3bb746153db3f973bd5c2b6e0de5ef110f767189512395b2de286f63747b6ee3f98c41e87541ecc8d0b1'
