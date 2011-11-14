9610694f77c71a628eaddbae819ec2be8ad1f667
Luis Vega <luiscvega@gmail.com>
2011-11-14 11:23:03 -0600

    Added mentor boolean to user indicating if he wants to be a mentor or not

--- app/controllers/apprenticeships_controller.rb
+++ app/controllers/apprenticeships_controller.rb
@@ -17,7 +17,27 @@ class ApprenticeshipsController < ApplicationController
   end
   
   def new
-    @available_students
-    @available_mentors
+    @available_mentors = User.where(:mentor => true)
+    @available_mentors = User.where("id <> ?", current_user.id)
+    @unavailable_mentors = current_user.apprenticeships.map do |apprenticeship|
+      apprenticeship.mentor
+    end
+    
+    @available_mentors =   @available_mentors.reject do |mentor|
+      @unavailable_mentors.include?(mentor)
+    end
+  end
+  
+  def create
+    @apprenticeship = Apprenticeship.new
+    @apprenticeship.student = current_user
+    @apprenticeship.mentor = User.find(params[:id])
+
+    if @apprenticeship.save
+      redirect_to apprenticeships_url, :notice => "Apprenticeship created!!"
+    else
+      redirect_to new_apprenticeship_url, :notice => "Apprenticeship not established.."
+    end
+      
   end
 end
